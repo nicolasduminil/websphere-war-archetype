f047d25046f677d9b6ebab74613c986617045873
Nicolas Duminil <nicolas.duminil@simplex-software.fr>
2017-03-31 12:11:21 +0200

    Have added the archetype required files

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-ws/src/main/java/ObjectFactory.java
@@ -0,0 +1,59 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+package ${package};
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the ${package} package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: ${package}
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link TestStringInput }
+     * 
+     */
+    public TestStringInput createTestStringInput() {
+        return new TestStringInput();
+    }
+
+    /**
+     * Create an instance of {@link TestStringOutput }
+     * 
+     */
+    public TestStringOutput createTestStringOutput() {
+        return new TestStringOutput();
+    }
+
+    /**
+     * Create an instance of {@link TestStringFault }
+     * 
+     */
+    public TestStringFault createTestStringFault() {
+        return new TestStringFault();
+    }
+
+}
---
+++ src/main/resources/archetype-resources/__rootArtifactId__-ws/src/main/java/package-info.java
@@ -0,0 +1,5 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.alphacredit.be/tests/")
+package ${package};
